41ddb813ae2a29112a48e448a27b9973d0247674
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-27 15:57:19 -0300

    up

--- Bootstrap-Café/script.js
+++ Bootstrap-Café/script.js
@@ -4,4 +4,4 @@ const elemento = document.querySelector('body')
 inputCheck.addEventListener('click', () => {
     const modo = inputCheck.checked ? 'dark' : 'light'
     elemento.setAttribute("data-bs-theme", modo)
-})
+});
